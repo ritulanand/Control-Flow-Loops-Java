a8fdd0d6f77b3519e0e10f94f68e380de4c2ad0b
ritulanand <ritulanand007@gmail.com>
2025-08-08 09:04:39 +0530

    priority queue intial setup added

---
+++ PriorityQueue.java
@@ -0,0 +1,41 @@
+ class PriorityQueue {
+
+    public class Element<T>{
+        T value;
+        int priority;
+        public Element(T value, int priority) {
+            this.value = value;
+            this.priority = priority;
+        }
+    }
+
+    public class Priority_Queue<T>{
+        private ArrayList<Element<T>> heap;
+        public Priority_Queue() {
+            heap = new ArrayList<>();
+        }
+
+
+        public void insert(T value, int priority) {
+           
+        }
+
+        public T getMin(){
+
+        }
+
+        public T removeMin() {
+            
+        }
+
+        public int size(){
+
+        }
+        public boolan isEmpty(){
+            
+        }
+    }
+    public static void main(String[] args) {
+        
+    }
+}
